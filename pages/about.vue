6b8e75665fd08c5bc91523ba73d95e94bb2fc308
JMathRA <j.matheusalbuquerque1@gmail.com>
2023-11-17 14:11:15 -0300

    Nuxtejs Project

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+      <h2>About</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
+        perspiciatis totam! Sapiente similique tenetur autem ea accusantium
+        nesciunt voluptatem. Nemo placeat perspiciatis accusantium exercitationem
+        debitis inventore dicta consequatur in itaque.
+      </p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped></style>
+  
---
+++ pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Products details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat
+      fugit porro, facere cumque temporibus unde totam magnam id quasi assumenda
+      esse quas et iste voluptatum? Perspiciatis voluptates quae saepe?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+definePageMeta({
+    layout: 'products'
+  })
+</script>
+
+<style scoped></style>
